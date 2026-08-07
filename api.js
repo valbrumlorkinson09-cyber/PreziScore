82d53e9a2bd2780bc61a9cfa994e580b6f4183f1
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-08-06 22:20:05 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,3 @@
-// FOOTBALL GLOBAL API
-
 const API_KEY = "47f671279defefb2b169097f1062a2a6";
 
 const API_URL = "https://v3.football.api-sports.io";
@@ -7,12 +5,14 @@ const API_URL = "https://v3.football.api-sports.io";
 
 async function getMatches(){
 
+const container = document.getElementById("liveMatches");
+
+
 try{
 
 const response = await fetch(
 `${API_URL}/fixtures?live=all`,
 {
-method:"GET",
 headers:{
 "x-apisports-key": API_KEY
 }
@@ -22,15 +22,62 @@ headers:{
 
 const data = await response.json();
 
-console.log(data);
+
+container.innerHTML="";
+
+
+if(data.response.length === 0){
+
+container.innerHTML =
+"<p>Aucun match en direct actuellement.</p>";
+
+return;
+
+}
+
+
+data.response.forEach(match=>{
+
+
+container.innerHTML += `
+
+<div class="match-card">
+
+<h3>
+${match.teams.home.name}
+</h3>
+
+<p>
+${match.goals.home} - ${match.goals.away}
+</p>
+
+<h3>
+${match.teams.away.name}
+</h3>
+
+<span>
+🔴 En direct
+</span>
+
+</div>
+
+`;
+
+
+});
+
 
 }
 
 catch(error){
 
-console.log("Erreur API:", error);
+container.innerHTML =
+"<p>Erreur de connexion API.</p>";
+
+console.log(error);
 
 }
+
 
 }
 
