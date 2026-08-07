f1c9b97f244a7995a50e808b11b712000a40458d
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-08-06 22:14:16 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,38 @@
+// FOOTBALL GLOBAL API
 
+const API_KEY = "47f671279defefb2b169097f1062a2a6";
+
+const API_URL = "https://v3.football.api-sports.io";
+
+
+async function getMatches(){
+
+try{
+
+const response = await fetch(
+`${API_URL}/fixtures?live=all`,
+{
+method:"GET",
+headers:{
+"x-apisports-key": API_KEY
+}
+}
+);
+
+
+const data = await response.json();
+
+console.log(data);
+
+}
+
+catch(error){
+
+console.log("Erreur API:", error);
+
+}
+
+}
+
+
+getMatches();
